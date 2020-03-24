1f243bda3f6886d980c6577577fbba9d0c89d72f
Eduardo Henrique <edu.ed322@gmail.com>
2020-03-24 18:31:43 -0300

    app

---
+++ app/src/main/java/br/com/webview/mangadex/MainActivity.kt
@@ -0,0 +1,84 @@
+package br.com.webview.mangadex
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.SharedPreferences
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val baseURL = "https://mangadex.org/"
+    private val prefs: SharedPreferences by lazy {
+        applicationContext.getSharedPreferences(
+            applicationContext.packageName,
+            Activity.MODE_PRIVATE
+        )
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        swipeRefresh.setOnRefreshListener {
+            webView.reload()
+        }
+
+        // WebView
+
+        webView.webChromeClient = object : WebChromeClient() {
+
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+
+
+                swipeRefresh.isRefreshing = true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                swipeRefresh.isRefreshing = false
+            }
+
+        }
+
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.setAppCacheEnabled(true)
+        settings.domStorageEnabled = true
+        settings.builtInZoomControls = true
+        settings.displayZoomControls = false
+        webView.loadUrl(prefs.getString("lastUrl", baseURL))
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+            super.onBackPressed()
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+
+        val edit = prefs.edit()
+        edit.putString("lastUrl", webView.url)
+        edit.apply()
+    }
+}
